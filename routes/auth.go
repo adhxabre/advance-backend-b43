87a62a7b03911d7c61dab25308a67964f397e3d9
adhxabre <abeldustin06@gmail.com>
2023-01-26 16:31:41 +0700

    Bcrypt

--- routes/auth.go
+++ routes/auth.go
@@ -1,5 +1,19 @@
 // Create package routes here ...
+package routes
 
 // import the required packages here ...
+import (
+	"dumbmerch/handlers"
+	"dumbmerch/pkg/mysql"
+	"dumbmerch/repositories"
+
+	"github.com/gorilla/mux"
+)
 
 // Create AuthRoutes function and add "/register" route with handler Register and POST method here ...
+func AuthRoutes(r *mux.Router) {
+	userRepository := repositories.RepositoryUser(mysql.DB)
+	h := handlers.HandlerAuth(userRepository)
+
+	r.HandleFunc("/register", h.Register).Methods("POST")
+}
